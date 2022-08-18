91ecd189b4266523e6b111f1fabf6f7f22ed6537
eknall <51799857+eknall@users.noreply.github.com>
2022-08-18 18:15:26 -0400

    corrected setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = "sividl",
   packages = ["sividl"],
-  py_modules=["wvgsolver.sividl_devices", "wvgsolver.sividl_utils"],
+  py_modules=["sividl.sividl_devices", "sividl.sividl_utils"],
   version = "0.2",
   author = "Can Knaut, Erik Knall",
   author_email = "cknaut@g.harvard.edu",
